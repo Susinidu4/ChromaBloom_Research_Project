38bc8dff30ed0979ccf51db05a198f1cd186308d
Wijerathna A.D.S.S <155086679+Susinidu4@users.noreply.github.com>
2025-11-10 00:28:49 +0530

    Initial project upload

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to Database
+connectDB();
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js Backend!");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
