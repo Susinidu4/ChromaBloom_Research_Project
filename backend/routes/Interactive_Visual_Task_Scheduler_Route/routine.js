4e3027d4a1a019f2de95edca0a58117b3ee0d461
Wijerathna A.D.S.S <155086679+Susinidu4@users.noreply.github.com>
2025-11-22 19:21:20 +0530

    create routine route related controllers

--- backend/routes/Interactive_Visual_Task_Scheduler_Route/routine.js
+++ backend/routes/Interactive_Visual_Task_Scheduler_Route/routine.js
@@ -1,8 +1,16 @@
 import express from 'express';
-import { createRoutine } from '../../controllers/Interactive_Visual_Task_Scheduler_Controller/routine.js';
+import { createRoutine, getRoutineByCreator, getRoutineById, updateRoutine, deleteRoutine } from '../../controllers/Interactive_Visual_Task_Scheduler_Controller/routine.js';
 
 const router = express.Router();
 
 router.post('/create', createRoutine);
+
+router.get('/getRoutine/:created_by', getRoutineByCreator);
+
+router.get('/getRoutineById/:routineId', getRoutineById);
+
+router.put('/updateRoutine/:routineId', updateRoutine);
+
+router.delete('/deleteRoutine/:routineId', deleteRoutine);
 
 export default router;
