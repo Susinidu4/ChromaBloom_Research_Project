67799000095d18cc251dcda8dd868316568b3fff
Yasindu Pasanjith <ypasanjith@gmail.com>
2025-11-26 22:57:19 +0530

    update child's controller and routes

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"; 
 import connectDB from "./config/db.js";
+import routine from "./routes/Interactive_Visual_Task_Scheduler_Route/routine.js";
 import adminRoutes from "./routes/Users/admin.routes.js";
 import caregiverRoutes from "./routes/Users/caregiver.routes.js";
-import routine from "./routes/Interactive_Visual_Task_Scheduler_Route/routine.js";
+import childRoutes from "./routes/Users/child.routes.js"
+
 
 dotenv.config();
 
@@ -28,6 +30,7 @@ app.use("/chromabloom/routine", routine);
 // Routes
 app.use("/chromabloom/api/admins", adminRoutes);
 app.use("/api/caregivers", caregiverRoutes);
+app.use("/api/children", childRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
