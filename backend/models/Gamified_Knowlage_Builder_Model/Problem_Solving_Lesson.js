3645e2985cd7a477bd7f1fd458532f798fe0aac0
Yasindu Pasanjith <141748340+OshiMC@users.noreply.github.com>
2025-12-01 00:34:41 +0530

    update new models

---
+++ backend/models/Gamified_Knowlage_Builder_Model/Problem_Solving_Lesson.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const TipsSchema = new mongoose.Schema({
+    tip_number: {type: Number, required: true},
+    tip_content: {type: String, required: true},
+});
+
+const ProblemSolvingLessonSchema = new mongoose.Schema({
+    _id: {type: String},
+    title: {type: String, required: true},
+    content: {type: String},
+    difficultyLevel: {type: String, enum: ['Easy', 'Medium', 'Hard'], required: true},
+    tips: [TipsSchema],
+}, {timestamps: true});
+    
+//pass unique id to _id field like LP-0001
+
+ProblemSolvingLessonSchema.pre('save', async function (next) {
+    if (this.isNew) {
+        const lastLesson = await mongoose.model('ProblemSolvingLesson').findOne().sort({_id: -1});
+        if (!lastLesson) {
+            this._id = 'LP-0001';
+        } else {
+            const lastNumber = parseInt(lastLesson._id.split('-')[1]);
+            this._id = 'LP-' + String(lastNumber + 1).padStart(4, '0');
+        }
+    }
+    next();
+});
+
+const ProblemSolvingLesson = mongoose.model('ProblemSolvingLesson', ProblemSolvingLessonSchema);
+export default ProblemSolvingLesson;
