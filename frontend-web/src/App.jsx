6994b82e355e07558702e19839e76aa0deca4f0b
Yasindu Pasanjith <128167079+YASSA2001@users.noreply.github.com>
2025-12-03 16:45:50 +0530

    update web

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -3,8 +3,10 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/other/home";
 import { AdminLogin } from "./pages/admin/Admin_login";
-import { TherapistsLogin } from "./pages/therapists/Therapists_login";
+import { TherapistsLogin } from "./pages/therapists/therapists_login";
 import { Admin_Dashboard } from "./pages/admin/Admin_Dashboard";
+import { Therapists_register } from "./pages/therapists/Therapists_register";
+import { Therapists_dashboard } from "./pages/therapists/Therapists_dashboard";
 
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
         <Route path="/admin_login" element={<AdminLogin />} />
         <Route path="/therapists_login" element={<TherapistsLogin />} />
         <Route path="/admin_dashboard" element={<Admin_Dashboard />} />
+        <Route path="/therapists_register" element={<Therapists_register />} />
+        <Route path="/therapists_dashboard" element={<Therapists_dashboard />} />
       </Routes>
     </>
   );
