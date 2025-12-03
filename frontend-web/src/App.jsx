697cc9eb1f85c77bc490264852592740836a5cac
Yasindu Pasanjith <128167079+YASSA2001@users.noreply.github.com>
2025-12-03 12:02:45 +0530

    create admin and therapists login pages

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/other/home'
+import { admin_login } from './pages/admin/admin_login'
+import { therapists_login } from './pages/therapists/therapists_login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +12,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/home" element={<div>home</div>} />
+        <Route path="/admin_login" element={<admin_login />} />
+        <Route path="/therapists_login" element={<therapists_login />} />
       </Routes>
     </>
   )
---
+++ frontend-web/src/pages/therapists/therapists_login.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const therapists_login = () => {
+  return (
+    <div>therapists_login</div>
+  )
+}
