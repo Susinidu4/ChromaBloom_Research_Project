8cd5935e58e35c6748859cd349e2bf1b43fa85b4
Wijerathna.A.D.S.S <155086679+Susinidu4@users.noreply.github.com>
2025-12-09 12:41:04 +0530

    create user activity route for all crud operation

---
+++ backend/routes/Interactive_Visual_Task_Scheduler_Route/userActivityRoute.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { createUserActivity, deleteUserActivity, updateUserActivity, getUserActivitiesByDate, getAllUserActivities, getAllActivities } from '../../controllers/Interactive_Visual_Task_Scheduler_Controller/userActivityController.js';
+
+const router = express.Router();
+
+// Create a new user activity
+// POST /chromabloom/userActivities
+router.post("/createUserActivity", createUserActivity);
+
+// Display all activities
+// GET /chromabloom/userActivities/allUserActivity
+router.get("/allUserActivity", getAllActivities);
+
+// Display all user activities for a caregiver
+// GET /chromabloom/userActivities/allUserActivity/:caregiverId
+router.get("/allUserActivity/:caregiverId", getAllUserActivities);
+
+// Delete a user activity by ID
+// POST /chromabloom/userActivities/getByDate
+router.post("/getByDate", getUserActivitiesByDate);
+
+// Delete User Activity by ID
+// DELETE /chromabloom/userActivities/deleteUserActivity/:activityId
+router.delete("/deleteUserActivity/:activityId", deleteUserActivity);
+
+// Update User Activity by ID
+// PUT /chromabloom/userActivities/updateUserActivity/:activityId
+router.put("/updateUserActivity/:activityId", updateUserActivity);
+
+export default router;
