40f2281e88dddf882a6009df19da5e8891dd4f74
Yasindu Pasanjith <141748340+OshiMC@users.noreply.github.com>
2025-12-16 13:21:07 +0530

    update adminContrllers

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/other/home";
-import { AdminLogin } from "./pages/admin/Admin_login";
 import { TherapistsLogin } from "./pages/therapists/therapists_login";
 import { Admin_Dashboard } from "./pages/admin/Admin_Dashboard";
 import { Therapists_register } from "./pages/therapists/Therapists_register";
 import { Therapists_dashboard } from "./pages/therapists/Therapists_dashboard";
+import CreateAdmin from "./pages/admin/CreateAdmin";
+import AdminLogin from "./pages/admin/admin_login";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin_login" element={<AdminLogin />} />
+        <Route path="/create_admin" element={<CreateAdmin />} />
         <Route path="/therapists_login" element={<TherapistsLogin />} />
         <Route path="/admin_dashboard" element={<Admin_Dashboard />} />
         <Route path="/therapists_register" element={<Therapists_register />} />
