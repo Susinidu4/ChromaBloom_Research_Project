a1319859d60c0cb72d899c834dbc7350f6251bca
Wijesundara J.A.I.S <156680354+Ishara184@users.noreply.github.com>
2025-12-28 20:25:06 +0530

    create digital wellbeing log model

---
+++ backend/models/Parental_Stress_Monitoring_Model/digitalWellbeingLogModel.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const digitalWellbeingLogSchema = new Schema(
+    {
+        caregiverId: {
+            type: String,
+            ref: "caregiver",
+            required: true,
+        },
+
+        log_date: {
+            type: Date,
+            required: true,
+        },
+
+        total_screen_time_min: {
+            type: Number,
+            required: true,
+        },
+
+        night_usage_min: {
+            type: Number,
+            required: true,
+        },
+
+        unlock_count:{
+            type: Number,
+            required: true,
+        },
+
+        app_opened_times_count:{
+            type: Number,
+            required: true,
+        },
+
+        social_media_min:{
+            type: Number,
+            required: true,
+        },
+
+        video_apps_min:{
+            type: Number,
+            required: true,
+        },
+
+        late_night_usage_flag:{
+            type: Boolean,
+            required: true,
+        },
+
+        sleep_quality: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        collection: "DigitalWellbeingLog",
+        timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+    }
+)
+
+const DigitalWellbeingLog = model("DigitalWellbeingLog", digitalWellbeingLogSchema);
+export default DigitalWellbeingLog;
