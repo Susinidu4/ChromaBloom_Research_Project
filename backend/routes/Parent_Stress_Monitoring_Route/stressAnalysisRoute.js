40973999f2658f4b4b060c60b202bec171da46c2
Wijesundara J.A.I.S <156680354+Ishara184@users.noreply.github.com>
2026-01-01 14:15:27 +0530

    add stress analysis route for parental stress monitoring

---
+++ backend/routes/Parent_Stress_Monitoring_Route/stressAnalysisRoute.js
@@ -0,0 +1,8 @@
+import express from 'express';
+//import { getConsentByCaregiver, saveConsentDecision } from '../../controllers/Parental_Stress_Monitoring_Controller/stressAnalysisController';
+
+const router = express.Router();
+
+
+
+export default router;
