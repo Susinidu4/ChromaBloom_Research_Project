18c35d64b7d25002ce4957fda9b1eb969c663390
Yasindu Pasanjith <128167079+YASSA2001@users.noreply.github.com>
2026-01-15 16:37:37 +0530

    update digital wellbegin controller ,  route and service

--- frontend-mobile/lib/services/Parental_stress_monitoring/digital_wellbeing_log_service.dart
+++ frontend-mobile/lib/services/Parental_stress_monitoring/digital_wellbeing_log_service.dart
@@ -6,6 +6,7 @@ class DigitalWellbeingService {
   static final String _base = ApiConfig.baseUrl;
 
   Future<void> createLog(Map<String, dynamic> payload) async {
+
     final uri = Uri.parse('$_base/chromabloom/digitalWellbeingLog/create');
 
     final res = await http.post(
@@ -16,6 +17,20 @@ class DigitalWellbeingService {
 
     if (res.statusCode < 200 || res.statusCode >= 300) {
       throw Exception('Create wellbeing log failed: ${res.statusCode} ${res.body}');
+    }
+  }
+
+  //get logs by caregiver id
+  Future<List<Map<String, dynamic>>> getLogsByCaregiverId(String caregiverId) async {
+    final uri = Uri.parse('$_base/chromabloom/digitalWellbeingLog/caregiver/$caregiverId');
+
+    final res = await http.get(uri);
+
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      final List<dynamic> data = jsonDecode(res.body);
+      return data.cast<Map<String, dynamic>>();
+    } else {
+      throw Exception('Get wellbeing logs failed: ${res.statusCode} ${res.body}');
     }
   }
 }
