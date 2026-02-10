c70aa0489f485c22ac52946292d725ff9cc4f924
Yasindu Pasanjith <128167079+YASSA2001@users.noreply.github.com>
2026-02-10 15:42:45 +0530

    create wellness page

---
+++ frontend-web/src/pages/Gemified_Knowledge_Builder/Wellness_Module.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminLayout from "../admin/Admin_Management/AdminLayout";
+
+export default function Wellness_Module() {
+    return (
+        <AdminLayout>
+            <div className="m-20">
+                <h1>Wellness Module</h1>
+            </div>
+        </AdminLayout>
+    );
+}
